f57cf3f5370c07e347739d9e6bf9b420c0d77f53
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-03-22 15:29:48 +0100

    + Added core structures

--- include/lemin.h
+++ include/lemin.h
@@ -10,8 +10,30 @@
 
 	#include "mylist.h"
 
-	typedef struct s_graph_node {
-		int id;
-		l_list_t *connected;
-	} graph_t;
+	typedef enum e_room_type {
+		DEFAULT = 0,
+		START = 1,
+		END = 2
+	} e_room_t;
+
+	typedef struct s_vector2i {
+		int x;
+		int y;
+	} vector2i_t;
+
+	typedef struct s_room {
+		char *name;
+		vector2i_t pos;
+		e_room_t type;
+		l_list_t *route;
+	} room_t;
+
+	typedef struct s_game {
+		int ant_nb;
+		l_list_t *rooms;
+		l_list_t *tunnels;
+
+	} game_t;
+
+	find_shortest_path();
 #endif
