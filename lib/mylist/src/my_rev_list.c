d56ead41a3c6be2e34bb48a39a9fc06308c4eebc
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-03-22 17:28:48 +0100

    * Fixed a mem leak

--- lib/mylist/src/my_rev_list.c
+++ lib/mylist/src/my_rev_list.c
@@ -10,11 +10,14 @@
 int my_rev_list(l_list_t **begin)
 {
 	l_list_t *cpy = NULL;
+	l_list_t *tmp = NULL;
 	int i = 0;
 
 	for (; cpy != 0; i++) {
 		push(&cpy, (*begin)->data);
+		tmp = *begin;
 		*begin = (*begin)->next;
+		free(tmp);
 	}
 	*begin = cpy;
 	return (i);
