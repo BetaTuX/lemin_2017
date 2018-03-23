0061239bce8923a93f2c5872f08942ddb0911c24
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-03-23 19:05:59 +0100

    * Fixed bfs

--- src/find_shortest_path.c
+++ src/find_shortest_path.c
@@ -13,15 +13,12 @@ l_list_t *find_first_path(room_t *start, int room_nb)
 	l_list_t *cur;
 	l_list_t *sec_cur;
 
+	push(&list, initialize_list(start, NULL));
 	start->marked = true;
-	for (sec_cur = start->tunnels; sec_cur; sec_cur = sec_cur->next) {
-		push(&list, initialize_list(sec_cur->data, NULL));
-		((room_t *)sec_cur->data)->marked = true;
-	}
-	for (l_list_t *bit = list; bit; bit = bit->next, my_printf("next\n"))
-		for (l_list_t *tib = bit->data; tib; tib = tib->next)
-			my_printf("%s\n", ((room_t *)tib->data)->name);
-	my_printf("end\n");
+	// for (l_list_t *bit = list; bit; bit = bit->next, my_printf("next\n"))
+	// 	for (l_list_t *tib = bit->data; tib; tib = tib->next)
+	// 		my_printf("%s\n", ((room_t *)tib->data)->name);
+	// my_printf("end\n");
 	for (int i = 0; i < room_nb; i++) {
 		for (cur = list; cur; cur = cur->next) {
 			sec_cur = ((room_t *)((l_list_t *)cur->data)->data)->tunnels;
@@ -32,16 +29,16 @@ sec_cur = sec_cur->next, r = (room_t *)(sec_cur) ? sec_cur->data : NULL);
 				((room_t *)sec_cur->data)->marked = true;
 				for (sec_cur = sec_cur->next; sec_cur; sec_cur = sec_cur->next) {
 					if (!(((room_t *)sec_cur->data)->marked)) {
-						push(&list, initialize_list(sec_cur->data, (cur->next) ? cur->next->data : NULL));
+						push(&list, initialize_list(sec_cur->data, ((l_list_t *)cur->data)->next));
 						((room_t *)sec_cur->data)->marked = true;
 					}
 				}
 			}
-			for (l_list_t *bit = list; bit; bit = bit->next, my_printf("next\n"))
-				for (l_list_t *tib = bit->data; tib; tib = tib->next)
-					my_printf("%s\n", ((room_t *)tib->data)->name);
-			my_printf("end\n");
-			my_printf("\"%s\" type : %d\n", ((room_t *)((l_list_t *)cur->data)->data)->name, ((room_t *)((l_list_t *)cur->data)->data)->type);
+			// for (l_list_t *bit = list; bit; bit = bit->next, my_printf("next\n"))
+				// for (l_list_t *tib = bit->data; tib; tib = tib->next)
+					// my_printf("%s\n", ((room_t *)tib->data)->name);
+			// my_printf("end\n");
+			// my_printf("\"%s\" type : %d\n", ((room_t *)((l_list_t *)cur->data)->data)->name, ((room_t *)((l_list_t *)cur->data)->data)->type);
 			if (cur && cur->data && ((l_list_t *)cur->data)->data && ((room_t *)((l_list_t *)cur->data)->data)->type == END) {
 				my_rev_list((l_list_t **)&(cur->data));
 				return ((l_list_t *)cur->data);
