710ada3f7219d7b4bc6acb8e77bd3138714d53e6
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-03-28 14:12:01 +0200

    * Modified main

--- src/main.c
+++ src/main.c
@@ -17,7 +17,8 @@ int main(UNUSED int argc, UNUSED char **argv)
 
 	if (fill_game(&game))
 		return (ERROR);
-	list = bfs(game.start, game.room_nb, NULL);
+	if (game.start == NULL || game.end == NULL)
+		list = bfs(game.start, game.room_nb, NULL);
 	if (list == NULL)
 		return (ERROR);
 	if (display_ants_and_room(&game, list) == ERROR)
